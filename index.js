60a912403ac35b64fe082b874a32205428e7469a
Farida Bano <159705091+farida-bano@users.noreply.github.com>
2024-03-22 12:31:00 +0500

    typescript
    typescript

---
+++ index.js
@@ -0,0 +1,28 @@
+/*//if
+const x: number = 10;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+}
+
+//if else
+const x: number = 3;
+
+if (x > 5) {
+    console.log("x is greater than 5");
+} else {
+    console.log("x is not greater than 5");
+}
+*/
+//else if
+const x = 7;
+if (x > 10) {
+    console.log("x is greater than 10");
+}
+else if (x > 5) {
+    console.log("x is greater than 5 but not greater than 10");
+}
+else {
+    console.log("x is less than or equal to 5");
+}
+export {};
